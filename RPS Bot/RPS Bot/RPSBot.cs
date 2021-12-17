d8f69bb043f242cb5e33e9cadd907c83a3795cc9
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-12-17 12:12:53 +0000

    the end of the FrancisIsABotTM Era

--- RPS Bot/RPS Bot/RPSBot.cs
+++ RPS Bot/RPS Bot/RPSBot.cs
@@ -8,22 +8,49 @@ namespace RPS_Bot
     public class RPSBot : IBot
     {
         const int maxDynamite = 100;
+        const int maxConsecutiveDynamite = 5;
         int usedDynamite = 0;
+        int consecutiveDynamite = 0;
+        bool canDynamite = true;
+
+        int firstHundred = 0;
         Dictionary<char, int> Moves = new Dictionary<char, int>();
         Move IBot.MakeMove(Gamestate gamestate)
         {
-            if (usedDynamite < maxDynamite)
+            if (usedDynamite < maxDynamite && consecutiveDynamite < maxConsecutiveDynamite && canDynamite && firstHundred > 100)
             {
                 usedDynamite++;
+                consecutiveDynamite++;
                 return Move.D;
-            }
-            else if (gamestate.GetRounds()[gamestate.GetRounds().Length - 1].GetP2() != Move.D)
-            {
-                return gamestate.GetRounds()[gamestate.GetRounds().Length - 1].GetP2();
             }
             else
             {
-                return Move.P;
+                canDynamite = false;
+                firstHundred++;
+                if (consecutiveDynamite == 0)
+                {
+                    canDynamite = true;
+                }
+                if (consecutiveDynamite > 0)
+                {
+                    consecutiveDynamite--;
+                }
+                try
+                {
+                    if (gamestate.GetRounds()[gamestate.GetRounds().Length - 2].GetP2() != Move.D)
+                    {
+                        return gamestate.GetRounds()[gamestate.GetRounds().Length - 2].GetP2();
+                    }
+                    else
+                    {
+                        return Move.W;
+                    }
+                }
+                catch
+                {
+                    return Move.W;
+                }
+
             }
         }
     }
