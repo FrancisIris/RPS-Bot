37d7d63131b3fbf0fec56a05528731b17756e1b8
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-12-17 11:34:25 +0000

    dynamite strategy implemented

--- RPS Bot/RPS Bot/RPSBot.cs
+++ RPS Bot/RPS Bot/RPSBot.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using BotInterface.Bot;
+using BotInterface.Game;
+using System;
+using System.Collections.Generic;
 
 namespace RPS_Bot
 {
-    public class Class1
+    public class RPSBot : IBot
     {
+        const int maxDynamite = 100;
+        int usedDynamite = 0;
+        Dictionary<char, int> Moves = new Dictionary<char, int>();
+        Move IBot.MakeMove(Gamestate gamestate)
+        {
+            if (usedDynamite < maxDynamite)
+            {
+                usedDynamite++;
+                return Move.D;
+            }
+            else if (gamestate.GetRounds()[gamestate.GetRounds().Length - 1].GetP2() != Move.D)
+            {
+                return gamestate.GetRounds()[gamestate.GetRounds().Length - 1].GetP2();
+            }
+            else
+            {
+                return Move.P;
+            }
+        }
     }
 }
